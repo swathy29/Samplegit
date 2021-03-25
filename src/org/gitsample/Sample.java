bf8cd53c27766cc4d2b9b9d5246656c0efb413ef
swathy29 <swathy2998@gmail.com>
2021-03-25 15:36:10 +0530

    first push

---
+++ src/org/gitsample/Sample.java
@@ -0,0 +1,16 @@
+package org.gitsample;
+
+public class Sample {
+	public class AxisBank{
+	   
+	    public void deposit() {
+	    	System.out.println("Deposit");
+	    	
+	    }
+	    public void main(String[] args) {
+			AxisBank a=new AxisBank();
+			a.deposit();
+				
+	    }
+	}
+}
